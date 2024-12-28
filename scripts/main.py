770f7efff8033478fcc28540aa5cc3f8aaf6841e
nstuh <n.stuhrmann@gmail.com>
2024-12-28 08:44:06 +0100

    - changed prompt to include length contraint - added tag insteadt of title suffix

--- scripts/main.py
+++ scripts/main.py
@@ -38,6 +38,45 @@ def update_document_title(doc_id, new_title):
     else:
         logging.info(f"Successfully updated document {doc_id} title to '{new_title}'")
 
+def update_document_tag(doc_id, tag_id):
+    url = f"{os.environ['PAPERLESS_URL']}/documents/bulk_edit/"
+
+    headers = {
+        "Authorization": f"Token {os.environ['PAPERLESS_API_KEY']}",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "documents": [doc_id],
+        "method": "add_tag",
+        "parameters": {
+            "tag": tag_id}
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    if response.status_code != 200:
+        logging.error(f"Error updating document tag: {response.status_code}")
+    else:
+        logging.info(f"Successfully add tag to document {doc_id}")
+
+
+def get_tag_id(tag):
+    url = f"{os.environ['PAPERLESS_URL']}/tags/?name__iexact={tag}"
+
+    headers = {
+        "Authorization": f"Token {os.environ['PAPERLESS_API_KEY']}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        logging.error(f"Error updating document title: {response.status_code}")
+    else:
+        logging.info(f"Successfully got tags")
+
+
+    return response.json()['results'][0]['id']
+
+
 def main():
     check_interval = int(os.environ.get('CHECK_INTERVAL', 60)) * 60
 
@@ -45,15 +84,16 @@ def main():
     with open(prompt_file, 'r') as f:
         prompt = f.read()
 
-    
+    filter_tag = os.environ.get('TAG_FILTER', 'inbox')
+
     while True:
-        print("Fetching documents...")
+        logging.info("Fetching documents...")
 
         results = list()
         response = None
 
         while True:
-            response = fetch_documents(response)
+            response = fetch_documents(response, tag=filter_tag)
 
             if not response:
                 break
@@ -73,6 +113,8 @@ def main():
         if len(matches) > 0:
             logging.info(f"Need to process {len(matches)} documents.")
 
+            tag_id = get_tag_id(os.environ['AUTO_TAG'])
+
             bot = RunPodChatBot.RunPodChatBot(prompt)
             new_title = dict()
 
@@ -80,16 +122,21 @@ def main():
                 logging.info(f"Processing document ID {doc['id']} ({i}/{len(matches)}) with title: {doc['title']}")
                 content = doc.get('content', '')
                 new_title[doc['id']] = bot.get_response(content)
+                #new_title[doc['id']] = [doc['title'], ]
 
             bot.terminate_pod()
             del bot
 
             logging.info('Updating titles')
-            for id, title in new_title.items():
-                logging.info(f'Updating document {id}, new title: {title}')
+            for i, (id, title) in enumerate(new_title.items()):
+                logging.info(f'Updating document {id} ({i}/{len(new_title)}), new title: {title}')
                 if title:
-                    update_document_title(id, title[0] + os.environ['AUTO_TITLE_SUFFIX'])
+                    update_document_tag(id, tag_id)
+                    update_document_title(id, title[0])
 
+
+        if check_interval == 0:
+            break
 
         logging.info(f"Sleeping for {check_interval} seconds...")
         time.sleep(check_interval)
